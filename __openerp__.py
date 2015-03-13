2523d178c49449dc77cb268967abdef23d08cdfe
wangting <39181819@qq.com>
2015-03-13 13:59:01 +0800

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+__author__ = 'wangting'
+{
+    'name': "Manufacure Order Unfinished Count",
+    'version': '1.0',
+    'depends': ['mrp'],
+    'author': "wangting",
+    'category': 'custom',
+    'data': [
+
+    ],
+    'application': True,
+    'description': """
+        Manufacure Order Unfinished Count
+    """
+}
